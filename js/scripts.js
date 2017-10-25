074fec1e62d342ef6065a33ba7950feb8a6a29f3
Chris Quattrocchi and Kevin Boyle <chris@>
2017-10-25 15:06:27 -0700

    begin to separate business and user logic

--- js/scripts.js
+++ js/scripts.js
@@ -1,65 +1,42 @@
-// break phrase into individual words
-// store words into an array
-// remove first character from word and put it on the end
-// if vowel is first letter, add -way to end
-// control for qu, sh, ch, th
-// add -ay to end of new word
 
 $(document).ready(function(){
   $("#formTranslate").submit(function(event){
-        event.preventDefault();
-    var vowels = ["a","e","i","o","u"];
-    var compoundLetters = ["qu", "sh", "ch", "th", "sch"]
+    event.preventDefault();
+
     var phraseString = $("input#phrase").val().toLowerCase();
-    // var first = phraseString.slice(0,1);
-    // var firstTwo = phraseString.slice(0,2);
+    var vowels = ["a","e","i","o","u"];
     var phraseArray = phraseString.split(" ");
     var wordSlice = phraseArray.slice(0,1);
     var wordSliceString = wordSlice.toString();
-    // var wordWithoutFirst = wordSliceString.slice(1);
-    // var wordWithoutFirstTwo = wordSliceString.slice(2);
     var letterSlice = Array.from(wordSlice[0]);
 
-
-// var reply = phraseArray.map(function(word) {
 phraseArray.forEach(function(word) {
   var first = word.slice(0,1);
   var firstTwo = word.slice(0,2);
   var wordWithoutFirst = word.slice(1);
   var wordWithoutFirstTwo = word.slice(2);
 
-// (first === "a" || first === "e" || first === "i" || first === "o" || first === "u")
 if  (vowels.indexOf(word[0]) > -1) {
-  console.log(word.concat("way"));
-  $(".placeholder").append(word.concat("way "));
+  word = word.concat("way ");
+  // console.log(word + "vowel")
+  // $(".placeholder").append(word.concat("way "));
 } else if (first <= 9) {
-  console.log(word);
-  $(".placeholder").append(word + " ");
+  word = word.concat(" ");
+  // console.log(word + "number")
+  // $(".placeholder").append(word + " ");
 } else if (firstTwo === "qu" || firstTwo === "sh" || firstTwo === "ch" || firstTwo === "th") {
-  console.log(wordWithoutFirstTwo.concat(firstTwo + "ay"));
-  $(".placeholder").append(wordWithoutFirstTwo.concat(firstTwo + "ay "));
+  word = wordWithoutFirstTwo.concat(firstTwo + "ay ");
+  // console.log(word + "qushch");
+  // $(".placeholder").append(wordWithoutFirstTwo.concat(firstTwo + "ay "));
 } else if (first !== "a" && first !== "e" && first !== "i" && first !== "o" && first !== "u") {
-  console.log(wordWithoutFirst.concat(first + "ay"));
-  $(".placeholder").append(wordWithoutFirst.concat(first + "ay "));
+  word = wordWithoutFirst.concat(first + "ay ");
+  // console.log(word + "everything else");
+  // $(".placeholder").append(wordWithoutFirst.concat(first + "ay "));
+
 };
+
+$(".placeholder").append(word);
 });
-
-// (var index = 0; index < phraseArray.length; index++)
-    // console.log("first " + first);
-    // console.log("phraseArray " + phraseArray);
-    // console.log("wordSlice " + wordSlice);
-    // console.log("letterSlice " +letterSlice);
-    // console.log("wordWithoutFirst " + wordWithoutFirst);
-
-
-  /////////////////////////////
-// if (first === "a" || first === "e" || first === "i" || first === "o" || first === "u") {
-//   console.log(phraseArray.concat() + "way");
-// } else if (firstTwo === "qu" || firstTwo === "sh" || firstTwo === "ch" || firstTwo === "th") {
-//   console.log(wordWithoutFirstTwo.concat(firstTwo + "ay"));
-// } else if (first !== "a" || first !== "e" || first !== "i" || first !== "o" || first !== "u") {
-//   console.log(wordWithoutFirst.concat(first + "ay"));
-// }
 
   });//submit function end
 });//doc ready end
