59dd7f04e87fd79fb9e4306bbab2e335e2a9fee5
Chris Quattrocchi and Kevin Boyle <chris@>
2017-10-25 16:20:13 -0700

    attempt to separate business vs. ui logic, some success

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,8 @@
-
 $(document).ready(function(){
   $("#formTranslate").submit(function(event){
     event.preventDefault();
 
+  // var pigLatin = function(phraseString) {
     var phraseString = $("input#phrase").val().toLowerCase();
     var vowels = ["a","e","i","o","u"];
     var phraseArray = phraseString.split(" ");
@@ -10,33 +10,38 @@ $(document).ready(function(){
     var wordSliceString = wordSlice.toString();
     var letterSlice = Array.from(wordSlice[0]);
 
-phraseArray.forEach(function(word) {
+  // var sentence = [];
+  phraseArray.forEach(function(word) {
   var first = word.slice(0,1);
   var firstTwo = word.slice(0,2);
   var wordWithoutFirst = word.slice(1);
   var wordWithoutFirstTwo = word.slice(2);
 
+
 if  (vowels.indexOf(word[0]) > -1) {
   word = word.concat("way ");
-  // console.log(word + "vowel")
-  // $(".placeholder").append(word.concat("way "));
+
+  // sentence.push(word1);
+  // alert(word1);
 } else if (first <= 9) {
   word = word.concat(" ");
-  // console.log(word + "number")
-  // $(".placeholder").append(word + " ");
+  // sentence.push(word2);
+  // alert(word2);
 } else if (firstTwo === "qu" || firstTwo === "sh" || firstTwo === "ch" || firstTwo === "th") {
   word = wordWithoutFirstTwo.concat(firstTwo + "ay ");
-  // console.log(word + "qushch");
-  // $(".placeholder").append(wordWithoutFirstTwo.concat(firstTwo + "ay "));
+  // sentence.push(word3);
+  // alert(word3);
 } else if (first !== "a" && first !== "e" && first !== "i" && first !== "o" && first !== "u") {
   word = wordWithoutFirst.concat(first + "ay ");
-  // console.log(word + "everything else");
-  // $(".placeholder").append(wordWithoutFirst.concat(first + "ay "));
+  // sentence.push(word4);
+  // alert(word4);
+  // return sentence.join(" ");
+  // console.log(sentence);
+}; //end of else if
 
-};
+ $(".placeholder").append(word);
+}); //end of forEach loop
 
-$(".placeholder").append(word);
-});
-
+  // alert(sentence);
   });//submit function end
 });//doc ready end
